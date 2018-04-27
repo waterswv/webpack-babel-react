7c008564ff6af7c5779c3fd4b359a4b8b2fb8863
Bryan <bryanmierke-macbookair@Bryans-MacBook-Air.local>
2018-04-27 13:17:28 -0700

    implementing a Higher Order Component

--- src/js/App.js
+++ src/js/App.js
@@ -10,10 +10,43 @@ export default class Hello extends Component {
     return (
       <div>
         Hello from react
-        <SearchableList />
+        <TodoList
+        todos={['Make Dinner', 'Finish Laundry', 'Make HOCs']}
+        isLoadingTodos={false}
+        />
       </div>
     );
   }
+}
+function TodoItem({ todo }) {
+  return <li>{todo}</li>
+}
+function TodoList({ todos, isLoadingTodos }) {
+  if (isLoadingTodos) {
+    return (
+      <div>
+        <p>Loading todos ...</p>
+      </div>
+    );
+  }
+
+  // Removed conditional rendering with null check
+
+  if (!todos.length) {
+    return (
+      <div>
+        <p>You have no Todos.</p>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <ul>
+        {todos.map((todo, index) => <TodoItem key={index} todo={todo} />)}
+      </ul>
+    </div>
+  );
 }
 
 render(<Hello />, document.getElementById('app'));
